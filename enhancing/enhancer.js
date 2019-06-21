df7f977738cc439d617bee43b2704e96eb0fa26f
jal9626 <jal9626@gmail.com>
2019-06-21 14:58:08 -0400

    mvp finished

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,22 +6,58 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item }
+  const newItem = {
+    name: item.name,
+    durability: item.durability,
+    enhancement: item.durability + 1
+  };
+  return newItem;
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement -5
+    }
+    return newItem;
+  }
+  else if (item.enhancement > 16) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement -1
+    }
+    return newItem;
+  }
+  else if (item.enhancement === 15 || 16) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement -10
+    }
+    return newItem;
+  }
 }
 
 function repair(item) {
-  const x = item.durability
-  const y = x - 100
-  const z = y + x
-  return {
-    ...item,
-    durability: z,
-    }
-  } 
+  // const x = item.durability
+  // const y = x - 100
+  // const z = y + x
+  // return {
+  //   ...item,
+  //   durability: z,
+  //   }
+  const newItem = { 
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+};
+
+  return newItem;
+
+} 
 
 
 function get(item) {
